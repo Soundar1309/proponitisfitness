f817620184c9f8963d105b065f313eae98fa27d1
Soundar Rajan <soundar1309@gmail.com>
2026-09-23 03:47:30 +0530

    feat: add robots and sitemap configuration files for SEO

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,11 +1,23 @@
 import { MetadataRoute } from "next";
 
+export const dynamic = "force-static";
+
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: "Googlebot",
+        allow: "/",
+      },
+      {
+        userAgent: "Googlebot-Image",
+        allow: "/",
+      },
+    ],
     sitemap: "https://proponitisfitness.com/sitemap.xml",
   };
 }
